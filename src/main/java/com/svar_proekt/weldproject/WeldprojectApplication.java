bb503c85e751cdf4161b365201f23ab5c4dcb959
Ibragim <gumakov111@gmail.com>
2025-05-23 13:25:42 +0300

    created a new project

---
+++ src/main/java/com/svar_proekt/weldproject/WeldprojectApplication.java
@@ -0,0 +1,13 @@
+package com.svar_proekt.weldproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeldprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeldprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svar_proekt/weldproject/WeldprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svar_proekt.weldproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeldprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
